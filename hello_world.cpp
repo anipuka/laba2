065dae36cc2b08de45e0f0d81658e961fc19544f
Anna <annakupina2006@yandex.ru>
2025-05-07 21:39:42 +0300

    added comment

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,11 @@
+//программа ввода и вывода имени пользователя
 #include <iostream>
-#include <string>
+   #include <string> 
    int main() {
        std::string name;
        std::cout << "Введите ваше имя: ";
-       std::cin >> name;
+       std::cin >> name; //ввод имени пользователя
        std::cout << "Hello, world from @" << name << std::endl;
+     // Используем std::cout и std::endl для избежания конфликтов имен
        return 0;
    }
