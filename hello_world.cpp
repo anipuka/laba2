d4e204ec06691cff97cf472c9b66a0fead7a6051
Anna <annakupina2006@yandex.ru>
2025-05-07 21:07:28 +0300

    Prompt for user name and personalize greeting

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,10 @@
-include <iostream>
-> using namespace std;
-> int main() {
-> cout << "Hello world!";
-> }
+#include <iostream>
+>#include <string>
+>using namespace std;
+>int main() {
+>    string name;
+>    cout << "Введите ваше имя: ";
+>    cin >> name;
+>    cout << "Hello, world from @" << name << endl;
+>    return 0;
+>}
